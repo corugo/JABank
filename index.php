f66e57b4c10f7a7d8bc6eb836fbdf3943d78aad2
corugo <victor_hugo_ventura@hotmail.com>
2024-08-12 20:47:38 -0400

    Ajustes na tabela da página principal

--- index.php
+++ index.php
@@ -72,10 +72,10 @@
                 print "
                         </div>
                         <div class='d-flex justify-content-center'>
-                            <h1 class='display-5'>".$row["name"].$user_row["name"]."</h1>
+                            <h1 class='display-5 text-center'>".$row["name"].$user_row["name"]."</h1>
                         </div>
                         <div class='d-flex justify-content-center'>
-                            <h1 class='display-6'>".$user_rank."º com ".$user_cash." JAcoins</h1>
+                            <h1 class='display-6'>Está em ".$user_rank."º com ".$user_cash." JAcoins</h1>
                         </div>
                         <div class='d-flex justify-content-center'>";
             }
@@ -92,17 +92,15 @@
             if ($result->num_rows > 0) {
                 // Exibir os dados em uma tabela
                 echo "<table class='table w-auto table-hover table-responsive'>";
-                echo "<tr><th>ID</th><th>Data/Hora</th><th>Correntista</th><th>Operação</th><th>JACoins</th><th>Descrição</th><th>Operador</th></tr>";
+                echo "<tr><th>Data/Hora</th><th>Operação</th><th>JACoins</th><th>Operador</th></tr>";
                 while($row = $result->fetch_assoc()) {
                     echo "<tr".(($row["operacao"] === "remover")?" class='table-danger'":(($row["operacao"] === "adicionar")?" class='table-success'":" class='table-primary'")).">";
-                    echo "<td>".$row["id"]."</td>";
                     echo "<td>".$row["datahora"]."</td>";
-                    echo "<td>".$row["name"]."</td>";
                     echo "<td>".(($row["operacao"] === "remover")?"-":(($row["operacao"] === "adicionar")?"+":"")).$row["quantidade"]."</td>";
                     echo "<td>".$row["cash"]."</td>";
-                    echo "<td>".$row["descricao"]."</td>";
                     echo "<td>".$row["usuario"]."</td>";
                     echo "</tr>";
+                    echo "<tr".(($row["operacao"] === "remover")?" class='table-danger'":(($row["operacao"] === "adicionar")?" class='table-success'":" class='table-primary'"))."><td style='border-bottom: 2px solid black; padding: 10px;' colspan='4'>".$row["descricao"]."</td></tr>";
                 }
                 echo "</table>";
             } else {
